46a00d92e56c479486370370dcab5cb27cd2b072
nachazo78 <42350098+nachazo78@users.noreply.github.com>
2022-06-15 12:40:33 -0300

    Update and rename Set Power Plan to Balanced.Safe.cmd to Set Power Plan to high power.Safe.cmd

---
+++ Scripts/Enabled/Set Power Plan to high power.Safe.cmd
@@ -0,0 +1,3 @@
+@echo off
+echo Set Power Plan to High Power. This is done this way to allow user to setup performance by the sliding control on battery icon.
+powercfg.exe /setactive 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
